2dfba1a2093e0f89b7330584a567368d8df5aed7
YanjinZhu <yanjinzh6@gmail.com>
2015-06-30 17:54:13 +0800

    Add LoadFile function and some constants.

--- tools/util.go
+++ tools/util.go
@@ -3,10 +3,13 @@ package tools
 import (
 	"errors"
 	"fmt"
+	"os"
+	"path"
 )
 
 const (
 	DEFAULT_CONFIG_FILE = "/conf/config.json"
+	DEFAULT_BUFFER_SIZE = 2048
 )
 
 var (
@@ -31,4 +34,24 @@ func Println(a ...interface{}) (n int, err error) {
 	} else {
 		return 0, nil
 	}
+}
+
+func LoadFile(filePath string) (file *os.File, err error) {
+	file, err = os.OpenFile(filePath, os.O_RDWR, 0x0666)
+	if err != nil {
+		if os.IsExist(err) {
+			Println(err)
+		} else {
+			err = os.MkdirAll(path.Dir(filePath), 0x0666)
+			if err != nil {
+				Println(err)
+			} else {
+				file, err = os.Create(filePath)
+				if err != nil {
+					Println(err)
+				}
+			}
+		}
+	}
+	return
 }
