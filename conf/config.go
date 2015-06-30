c8b884d5f98c28f487ba38d6f172b49208e28a1e
YanjinZhu <yanjinzh6@gmail.com>
2015-06-30 17:55:36 +0800

    Add config.go, read a json file and return a config struct.

---
+++ conf/config.go
@@ -0,0 +1,63 @@
+package conf
+
+import(
+	"os"
+	"github.com/yanjinzh6/youzhe-server/tools"
+)
+
+type Config struct {
+	Server serverConfig
+	Redis redisConfig
+}
+
+type serverConfig struct {
+	Addr string
+	Port string
+}
+
+type redisConfig struct {
+	Addr string
+	Port string
+}
+
+var config *Config
+
+func InitConfig(filePath string) (conf *Config) {
+	if filePath == nil || filePath == "" {
+		filePath = tools.DEFAULT_CONFIG_FILE
+	}
+	if config == nil {
+		file, err := tools.LoadFile(filePath)
+		tools.Println(err)
+		reader := bufio.NewReaderSize(file, tools.DEFAULT_BUFFER_SIZE)
+		buf := make([]byte, 0, tools.DEFAULT_BUFFER_SIZE)
+		bufs := bytes.NewBuffer(buf)
+		for {
+			buf, err := reader.ReadBytes('\n')
+			tools.Println(buf)
+			bufs.Write(buf)
+			if err != nil {
+				break
+			}
+		}
+		tools.Println(bufs.Len())
+		if bufs.Len() == 0 {
+			//nothing
+			config = &Config{
+				Server: {Addr: "127.0.0.1", Port: "3333"},
+				Redis: {Addr: "127.0.0.1", Port: "6379"},
+			}
+			tools.Println(config)
+			writer := bufio.NewWriterSize(file, tools.DEFAULT_BUFFER_SIZE)
+			buf, err = json.Marshal(&config)
+			tools.Println(err)
+			n, err := writer.Write(buf)
+			tools.Println(n, err)
+			writer.Flush()
+		} else {
+			err = json.Unmarshal(bufs.Bytes(), &config)
+			tools.Println(err)
+		}
+	}
+	return config
+}
