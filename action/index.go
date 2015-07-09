af01e9f75d766ccc19555de97fd7befcf9bcac79
YanjinZhu <yanjinzh6@gmail.com>
2015-07-10 01:13:03 +0800

    use template parseFile to return the index.html page and when all request does not match, will return 404.html page;

--- action/index.go
+++ action/index.go
@@ -2,6 +2,7 @@ package action
 
 import (
 	"net/http"
+	"html/template"
 	"github.com/yanjinzh6/youzhe-server/tools"
 )
 
@@ -17,7 +18,13 @@ func init() {
 }
 
 func (i *index) Index(w http.ResponseWriter, r *http.Request) {
-	tools.Println(w.Header(), r.Host)
+	//http.Redirect(w, r, "template/index.html", http.StatusFound)
+	t, err := template.ParseFiles("template/index.html")
+	if err != nil {
+		tools.Println(err)
+	} else {
+		t.Execute(w, nil)
+	}
 }
 
 func (i *index) Demo(w http.ResponseWriter, r *http.Request) {
--- server/httpServer.go
+++ server/httpServer.go
@@ -3,11 +3,16 @@ package server
 import (
 	"reflect"
 	"net/http"
+	"html/template"
 	"github.com/yanjinzh6/youzhe-server/conf"
 	"github.com/yanjinzh6/youzhe-server/tools"
 )
 
 func InitServer() {
+	http.Handle("/css/", http.FileServer(http.Dir("template")))
+	http.Handle("/js/", http.FileServer(http.Dir("template")))
+	http.Handle("/img/", http.FileServer(http.Dir("template")))
+	http.Handle("/fonts/", http.FileServer(http.Dir("template")))
 	config := conf.InitConfig(tools.DEFAULT_CONFIG_FILE)
 	in := make([]reflect.Value, 2)
 	//http.HandleFunc(config.Server.HandlerList[0].Action, config.Server.HandlerList[0].MyFunc)
@@ -17,7 +22,7 @@ func InitServer() {
 			handler := func (w http.ResponseWriter, r *http.Request) {
 				in[0] = reflect.ValueOf(w)
 				in[1] = reflect.ValueOf(r)
-				tools.Println(in, "\n", w, "\n", r)
+				//tools.Println(in, "\n", w, "\n", r)
 				med.Call(in)
 			}
 			http.HandleFunc(ht.Action, handler)
@@ -25,8 +30,27 @@ func InitServer() {
 			tools.Println(err)
 		}
 	}
+	http.HandleFunc("/", NotFoundHandler)
 	err := http.ListenAndServe(":" + config.Server.Port, nil)
 	if (err != nil) {
 		tools.Println(err)
+	}
+}
+
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		t, err := template.ParseFiles("template/index.html")
+		if err != nil {
+			tools.Println(err)
+		} else {
+			t.Execute(w, nil)
+		}
+	} else {
+		t, err := template.ParseFiles("template/404.html")
+		if err != nil {
+			tools.Println(err)
+		} else {
+			t.Execute(w, nil)
+		}
 	}
 }
