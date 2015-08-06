ad583364a1d3da14d7ca9fc46877b54630868740
YanjinZhu <yanjinzh6@gmail.com>
2015-08-06 19:09:07 +0800

    add new menu directive.

--- template/js/controllers/sessionController.js
+++ template/js/controllers/sessionController.js
@@ -9,6 +9,7 @@ angular.module('yozh.controllers').
 
 controller('SessionController', ['$scope', '$location', '$routeParams', 'sessionService', function($scope, $location, $routeParams, sessionService){
 	// console.log($routeParams.item);
+	console.log($location);
 	$scope.menuItem = $routeParams.item;
 	$scope.curUser = {};
 	$scope.curMenu = '菜单1';
@@ -16,6 +17,6 @@ controller('SessionController', ['$scope', '$location', '$routeParams', 'session
 	$scope.showLogin = function () {
 		$location.path('/login');
 	}
-	$scope.userMenus = [{name: '菜单1', ico: 'icon-home', href: '#/menu/11'}, {name: '菜单2', ico: 'icon-flag', href: '#/menu/12'}, {name: '菜单3', ico: 'icon-earth', href: '#/menu/13'}, {name: '菜单4', ico: 'icon-bookmarks', href: '#/menu/14'}];
-	$scope.adminMenus = [{name: '管理菜单1', ico: 'icon-cog', href: '#/menu/21'}, {name: '管理菜单2', ico: 'icon-flag', href: '#/menu/22'}, {name: '管理菜单3', ico: 'icon-pie-chart', href: '#/menu/23'}, {name: '管理菜单4', ico: 'icon-users', href: '#/menu/24'}, {name: '管理菜单5', ico: 'icon-clipboard', href: '#/menu/25'}];
+	$scope.userMenus = [{name: '菜单1', ico: 'icon-home', href: '/menu/11'}, {name: '菜单2', ico: 'icon-flag', href: '/menu/12'}, {name: '菜单3', ico: 'icon-earth', href: '/menu/13'}, {name: '菜单4', ico: 'icon-bookmarks', href: '/menu/14'}];
+	$scope.adminMenus = [{name: '管理菜单1', ico: 'icon-cog', href: '/menu/21'}, {name: '管理菜单2', ico: 'icon-flag', href: '/menu/22'}, {name: '管理菜单3', ico: 'icon-pie-chart', href: '/menu/23'}, {name: '管理菜单4', ico: 'icon-users', href: '/menu/24'}, {name: '管理菜单5', ico: 'icon-clipboard', href: '/menu/25'}];
 }]);
---
+++ template/js/directives/commonMenu.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+* yozh.directives Module
+*
+* Description
+*/
+angular.module('yozh.directives', []).
+
+directive('menuItem', ['$location', function($location){
+	// Runs during compile
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		template: '<li ng-click="gotoView()" ng-class="{active: isActive}"><a ng-href="#[[menu.href]]" class=""><span class="[[menu.ico]]"></span>[[menu.name]]</a></li>',
+		// templateUrl: '',
+		replace: true,
+		transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		link: function($scope, iElm, iAttrs, controller) {
+			$scope.isActive = false;
+			console.log($location.path(), $scope.menu.href);
+			if ($location.path() == $scope.menu.href) {
+				$scope.isActive = true;
+			}
+			$scope.$watch(function() {
+				return $location.path();
+			}, function(value) {
+				if (value == $scope.menu.href) {
+					$scope.isActive = true;
+				} else {
+					$scope.isActive = false;
+				}
+			});
+		}
+	};
+}]);
