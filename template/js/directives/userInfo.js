daa17f99691e1a6e647ce378e3de95731546d50f
YanjinZhu <yanjinzh6@gmail.com>
2015-08-07 17:59:29 +0800

    add user information directive.

---
+++ template/js/directives/userInfo.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+* yozh.directives Module
+*
+* Description
+*/
+angular.module('yozh.directives').
+
+directive('userInfo', ['$location', function($location){
+	// Runs during compile
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		// template: '',
+		templateUrl: '../../views/userInfo.html',
+		// replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		link: function($scope, iElm, iAttrs, controller) {
+			
+		}
+	};
+}]);
